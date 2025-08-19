463846287823e09d5ee87972a78868f437a8a00e
LHSDias <luizhenriquemc15@gmail.com>
2025-08-19 12:34:14 -0300

    Game Seed

--- src/main/java/com/cefet/dslist/entities/Game.java
+++ src/main/java/com/cefet/dslist/entities/Game.java
@@ -1,5 +1,6 @@
 package com.cefet.dslist.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,25 +15,42 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String titulo;
+
+    @Column(nullable = false)
     private Integer ano;
+
+    @Column(nullable = false)
     private String genero;
+
+    @Column(nullable = false)
     private String plataforma;
+
+    @Column(nullable = false)
+    private Double score;
+
+    @Column(name = "img_url", nullable = false)
     private String imgUrl;
+
+    @Column(name = "curta_descricao", columnDefinition = "TEXT", nullable = false)
     private String curtaDescricao;
+
+    @Column(name = "longa_descricao", columnDefinition = "TEXT", nullable = false)
     private String longaDescricao;
 
     public Game() {
 
     }
 
-    public Game(Long id, String titulo, Integer ano, String genero, String plataforma, String imgUrl,
+    public Game(Long id, String titulo, Integer ano, String genero, String plataforma, Double score, String imgUrl,
             String curtaDescricao, String longaDescricao) {
         this.id = id;
         this.titulo = titulo;
         this.ano = ano;
         this.genero = genero;
         this.plataforma = plataforma;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.curtaDescricao = curtaDescricao;
         this.longaDescricao = longaDescricao;
@@ -78,6 +96,14 @@ public class Game {
         this.plataforma = plataforma;
     }
 
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
     public String getImgUrl() {
         return imgUrl;
     }
@@ -107,6 +133,14 @@ public class Game {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+        result = prime * result + ((ano == null) ? 0 : ano.hashCode());
+        result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+        result = prime * result + ((plataforma == null) ? 0 : plataforma.hashCode());
+        result = prime * result + ((score == null) ? 0 : score.hashCode());
+        result = prime * result + ((imgUrl == null) ? 0 : imgUrl.hashCode());
+        result = prime * result + ((curtaDescricao == null) ? 0 : curtaDescricao.hashCode());
+        result = prime * result + ((longaDescricao == null) ? 0 : longaDescricao.hashCode());
         return result;
     }
 
@@ -124,8 +158,50 @@ public class Game {
                 return false;
         } else if (!id.equals(other.id))
             return false;
+        if (titulo == null) {
+            if (other.titulo != null)
+                return false;
+        } else if (!titulo.equals(other.titulo))
+            return false;
+        if (ano == null) {
+            if (other.ano != null)
+                return false;
+        } else if (!ano.equals(other.ano))
+            return false;
+        if (genero == null) {
+            if (other.genero != null)
+                return false;
+        } else if (!genero.equals(other.genero))
+            return false;
+        if (plataforma == null) {
+            if (other.plataforma != null)
+                return false;
+        } else if (!plataforma.equals(other.plataforma))
+            return false;
+        if (score == null) {
+            if (other.score != null)
+                return false;
+        } else if (!score.equals(other.score))
+            return false;
+        if (imgUrl == null) {
+            if (other.imgUrl != null)
+                return false;
+        } else if (!imgUrl.equals(other.imgUrl))
+            return false;
+        if (curtaDescricao == null) {
+            if (other.curtaDescricao != null)
+                return false;
+        } else if (!curtaDescricao.equals(other.curtaDescricao))
+            return false;
+        if (longaDescricao == null) {
+            if (other.longaDescricao != null)
+                return false;
+        } else if (!longaDescricao.equals(other.longaDescricao))
+            return false;
         return true;
     }
+
+    
 
     
 }
